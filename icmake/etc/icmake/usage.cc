30562a4a7a86aa15fb487ae64e31345a5994fd5f
Frank B. Brokken <f.b.brokken@rug.nl>
2006-12-03 13:52:52 +0000

    Cleanup of icmstart/icmbuild scripts in progress
    git-svn-id: https://icmake.svn.sourceforge.net/svnroot/icmake/icmake/trunk@61 dcc949cb-2a1c-0410-b67a-e7e5ca830264

---
+++ icmake/etc/icmake/usage.cc
@@ -0,0 +1,18 @@
+//                     usage.cc
+
+#include "main.ih"
+
+void usage(std::string  const  &progname)
+{
+    cout << "\n" <<
+    progname << " by " << Icmbuild::author <<
+    progname << " V" << Icmbuild::version << " " << Icmbuild::year << "\n"
+    "\n"
+    "Usage: " << progname << " [options] args\n"
+    "Where:\n"
+    "   [options] - optional arguments (short options between parentheses):\n"
+    "      --help (-h)      - provide this help\n"
+    "      --version (-v)   - show version information and terminate\n"
+    "   args   - explain additional arguments.\n"
+    << endl;
+}
---
+++ icmake/etc/icmake/version.cc
@@ -0,0 +1,11 @@
+//                     version.cc
+
+#include "main.ih"
+
+namespace Icmbuild
+{
+    char version[]  = "0.00";
+    char year[]     = "2006";
+    
+    char author[] = "Frank B. Brokken (f.b.brokken@rug.nl)"
+}

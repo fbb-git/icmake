5d595b0fdd3605fac82dd8818ae2e02689bce628
Frank B. Brokken <f.b.brokken@rug.nl>
2007-01-12 16:57:04 +0000

    Reorganizing skeletons and icmbuild script
    git-svn-id: https://icmake.svn.sourceforge.net/svnroot/icmake/icmake/trunk@150 dcc949cb-2a1c-0410-b67a-e7e5ca830264

--- icmake/usr/share/icmake/version.cc
+++ icmake/usr/share/icmake/version.cc
@@ -7,5 +7,5 @@ namespace Icmbuild
     char version[]  = "0.00";
     char year[]     = "2006";
     
-    char author[] = "Frank B. Brokken (f.b.brokken@rug.nl)"
+    char author[] = "Frank B. Brokken (f.b.brokken@rug.nl)";
 }

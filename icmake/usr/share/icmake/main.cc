a2366fb3b47c07dd3b1b63053d0fc64508660b69
Frank B. Brokken <f.b.brokken@rug.nl>
2009-02-12 20:08:29 +0000

    WIP, mainly on icmstart
    git-svn-id: https://icmake.svn.sourceforge.net/svnroot/icmake/icmake/trunk@927 dcc949cb-2a1c-0410-b67a-e7e5ca830264

--- icmake/usr/share/icmake/main.cc
+++ icmake/usr/share/icmake/main.cc
@@ -1,38 +1,10 @@
 #include "main.ih"
 
-// Uncomment all commented out code if you want to use Bobcat's Arg class to
-// handle command-line options and arguments. (cf. man -e bobcat arg)
-// If you don't want to use Bobcat's Arg class all the commented out lines can
-// safely be removed.
-
-//#include <bobcat/arg>
-//using namespace FBB;
-//
-//namespace
-//{
-//    Arg::LongOption longOptions[] =
-//    {
-//        Arg::LongOption("help", 'h'),
-//        Arg::LongOption("version", 'v'),
-//    };
-//
-//    Arg::LongOption const *const longEnd =
-//                    longOptions +
-//                    sizeof(longOptions) / sizeof(Arg::LongOption);
-//}
-
 int main(int argc, char **argv)
 try
 {
-//    Arg &arg = Arg::initialize("hv", longOptions, longEnd, argc, argv);
-
     return 0;
 }
-//catch(Errno const &err)
-//{
-//    cerr << err.what() << endl;
-//    return err.which();
-//}
 catch (...)
 {
     return 1;

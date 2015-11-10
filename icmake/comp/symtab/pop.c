5d6c580d8a29d40069c7d380f28badd54b4c19a1
Frank B. Brokken <f.b.brokken@rug.nl>
2015-11-10 17:16:56 +0100

    WIP allowing for-stmnt to define variables

--- icmake/comp/symtab/pop.c
+++ icmake/comp/symtab/pop.c
@@ -1,6 +1,12 @@
+#define msg
+/*
+*/
+
 #include "symtab.ih"
 
 void symtab_pop()
 {
     free(gs_vars.varIndices[--gs_vars.nLevels].index);
+
+    msg("returning to symtab offset: %u", gs_vars.nLevels);
 }
--- icmake/comp/symtab/push.c
+++ icmake/comp/symtab/push.c
@@ -1,7 +1,13 @@
+#define msg
+/*
+*/
+
 #include "symtab.ih"
 
 void symtab_push()
 {
+    msg("next symtab offset: %u", gs_vars.nLevels);
+
     if (gs_vars.nLevels == gs_vars.capacity)
         gs_vars.varIndices = rss_realloc(gs_vars.varIndices, 
                             (gs_vars.capacity += capacityInc) 

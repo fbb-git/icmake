7dc8ed8f485f0950cc57031f5997a082a8bfde87
Frank B. Brokken <f.b.brokken@rug.nl>
2015-11-21 17:42:44 +0100

    preparing the installation scripts

--- icmake/icmake/data.c
+++ icmake/icmake/data.c
@@ -1,15 +1,8 @@
 #include "icmake.ih"
 
-/*
 char icm_comp[]  = LIBDIR "/icm-comp";
 char icm_exec[]  = LIBDIR "/icm-exec";
 char icm_pp[]    = LIBDIR "/icm-pp";
-*/
-
-char icm_comp[]  = "/home/frank/src/icmake/icm-comp/tmp/bin/binary";
-char icm_exec[]  = "/home/frank/src/icmake/icm-exec/tmp/bin/binary";
-char icm_pp[]    = "/home/frank/src/icmake/icm-pp/tmp/bin/binary";
-
 
 char *imFile;
 char *pimFile;

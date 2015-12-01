7f8c4524bde6c3b7b986057fecdfbf2557d361ac
Frank B. Brokken <f.b.brokken@rug.nl>
2015-12-01 13:43:26 +0100

    fixed a bug in icm-exec/builtin/bsubstring.c

--- icmake/icm-exec/builtin/bsubstring.c
+++ icmake/icm-exec/builtin/bsubstring.c
@@ -3,24 +3,36 @@
 
 #include "builtin.ih"
 
+static char const *limits(char const *ptr, char const *begin, char const *end)
+{
+    if (ptr < begin)
+        return begin;
+    if (ptr > end)
+        return end;
+    return ptr;
+}
+
 void b_subString()
 {
+                                        /* begin/end limits of the str */
     char const *str = string_charp(stack_top());
+    char const *beyond = str + strlen(str);
 
-    size_t str_len = strlen(str);
+                                        /* requested begin/end ptrs of the 
+                                            substring */
+    char const *begin = str + int_value(stack_top() - 1);
+    char const *end   = begin + int_value(stack_top() - 2);
 
-    int firstIdx = int_value(stack_top() - 1);
-    int nChars = int_value(stack_top() - 2);
+                                        
+    begin = limits(begin, str, beyond); /* set begin inside str */
+    end = limits(end, str, beyond);     /* set end inside str */
 
-    if (firstIdx < 0)
-        firstIdx = 0;
-
-    if ((size_t)firstIdx < str_len && nChars > 0)
-    {
-        stringcons_charPtr(eb_releaseReg(), str + firstIdx);  /* intial copy */
-        string_reduce(&gb_reg, nChars);               /* reduce the strlen if 
+    if (end < begin)                    /* end must at least be begin */
+        end = begin;
+                    
+    stringcons_charPtr(eb_releaseReg(), begin);     /* intial copy */
+    string_reduce(&gb_reg, end - begin);            /* reduce the strlen if 
                                                        necessary */
-    }    
 }
 
 

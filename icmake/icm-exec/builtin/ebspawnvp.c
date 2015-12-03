aaa15634e14901ecab20328c2b6161041045eeeb
Frank B. Brokken <f.b.brokken@rug.nl>
2015-12-03 19:02:32 +0100

    removed fixed bufsize from icm-exec/builtin/ebspawnvp.c

--- icmake/icm-exec/builtin/ebspawnvp.c
+++ icmake/icm-exec/builtin/ebspawnvp.c
@@ -1,31 +1,31 @@
 #include "builtin.ih"
 
-int eb_spawnvp (int mode, char const *prog, char const **av)
+int eb_spawnvp(int mode, char const *prog, char const **av)
 {
-    char
-        buf [MAX_PATHLEN * 4];
+    size_t length = strlen(prog);   /* determine length of required buffer */
 
-    strcpy (buf, prog);
-    av++;
+    char const **begin = av + 1;        
+    while (*begin)
+        length += strlen(*begin++);
 
-    while (*av)
+                                    /* add 1 for ASCIIZ + # elements in av */
+    char *buf = rss_realloc(NULL, length + 1 + begin - av);
+
+    strcpy(buf, prog);              /* copy the program */
+    while (*++av)                   /* plus all blank delimited extra args */
     {
-        strcat (buf, " ");
-        strcat (buf, *av);
-        av++;
+        strcat(buf, " ");
+        strcat(buf, *av);
     }
 
-    return (system (buf));
+    return system(buf);             /* run the program */
 }
 
 #ifdef DEBUG
 int main ()
 {
-    static char
-        *args [] = { "ls", "*.c", "*.h", NULL };
+    static char *args[] = { "ls", "*.c", "*.h", NULL };
 
-    _spawnvp (0, "ls", (char const **) args);
-
-    return (0);
+    eb_spawnvp(0, "ls", (char const **) args);
 }
 #endif

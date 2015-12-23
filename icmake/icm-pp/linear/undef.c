9b040769a764fb4f71a993aba5182e0e63db281f
Frank B. Brokken <f.b.brokken@rug.nl>
2015-12-23 16:12:43 +0100

    On amd64 archs icm-pp/linear/undef.c caused a segfault.     This was fixed by simply erasing the item-to-undef's key (id).     This commit implements a better approach: the item-to-undef is     freed, linear's l_size is reduced, and if the removed item     didn't happen to be the last item, then the last item's id     and definition are copied to the now free spot.

--- icmake/icm-pp/linear/undef.c
+++ icmake/icm-pp/linear/undef.c
@@ -2,13 +2,21 @@
 
 int linear_undef(char const *ident)
 {
-    Linear const *item = linear_search(ident);
+    Linear *item = (Linear *)linear_search(ident);
 
     if (item == NULL)
         return 0;
 
-    *item->id = 0;
+    free(item->id);                         // free the item to undef
     free(item->definition);
+
+    --l_size;                               // reduce the size
+
+    if (l_table + l_size != item)           // not the last item was removed:
+    {
+        item->id = l_table[l_size].id;                  // store the last item
+        item->definition = l_table[l_size].definition;  // at the free spot
+    }
 
     return 1;
 }

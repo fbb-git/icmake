600be0d7305d9ac8bb1541f8dccd5d02a9ae17f8
Frank B. Brokken <f.b.brokken@rug.nl>
2016-09-03 15:26:06 +0200

    Updated the man-pages

--- icmake/icmake/usage.c
+++ icmake/icmake/usage.c
@@ -13,50 +13,65 @@ void usage(char const *program)
         " [options] source [dest] [-- [args]]\n"
         "where:\n"
         "    options:  optional arguments:\n"
-        "      -a     : information about ",
+        "      --about (-a)     : information about ",
             program,
         "\n"
-        "      -c     : the destination file is compiled (even if it's "
-                                                            "up-to-date)\n"
-        "      -e     : execute the .bim file, which is provided as "
-                                                            "`source'\n"
+        "      --compile (-c)   : the destination file is compiled "
+                                                "(even if it's up-to-date)\n"
+
+        "      --directory (-T) : the directory name following -T is used "
+                                                "for icmake's\n"
+        "               temporary files. By default /tmp is used or the "
+                                                "HOME\n"
+        "               directory if /tmp is not writable. When used, -T is\n"
+        "               usually specified as icmake's first option\n"
+
+        "      --execute (-e)   : execute the .bim file, which is provided "
+                                                "as `source'\n"
         "               (remaining arguments are passed as-is to the .bim\n"
         "               file)\n"
-        "      -f     : the icmake source file is recompiled (even if the\n"
+
+        "      --force (-f)     : the icmake source file is recompiled "
+                                                "(even if the\n"
         "               .bim file is up-to-date) either when no other "
-                                                                "options\n"
-        "               are specified, or when in combination with options\n"
-        "               -i and -t\n"
-        "      -F     : show filenames, flags, and actions to perform\n"
-        "      -h     : provide this help and end ",
-            program,
-                    "\n"
-        "      -i     : the first argument is the icmake source file to\n"
+                                                "options\n"
+        "               are specified, or when specified in combination "
+                                                "with options\n"
+        "               --source and --tmpbin\n"
+
+        "      --help (-h)      : provide this help and end ",
+                                                                program,
+
+        "      --icm-dep (-d)   : call icm-dep, passing it any remaining "
+                                                "arguments\n"
+
+        "      --preprocess (-p): only the preprocessor is activated\n"
+
+        "      --source (-i)    : the first argument is the icmake source "
+                                                "file to\n"
         "               process (default extension: .im), the default\n"
         "               .bim-filename is used and the .bim file is kept;\n"
         "               remaining arguments are passed to icm-exec\n"
-        "      -p     : only the preprocessor is activated\n"
-        "      -P     : call icm-dep, passing it the remaining arguments\n"
-        "      -t     : the filename following -t is the name of the "
-                                                                "compiled\n"
+
+        "      --summary (-F)   : show filenames, flags, and actions to "
+                                                "perform\n"
+
+        "      --tmpbin (-t)    : the filename following -t is the name of "
+                                                "the compiled\n"
         "               .bim-file (removed on exit). The first argument is "
-                                                                "the\n"
+                                                "the\n"
         "               icmake source file to process (using default "
-                                                                "extension\n"
+                                                "extension\n"
         "               .im). Remaining arguments are passed as-is to "
-                                                                "icm-exec\n"
-        "      -T     : the directory name following -T is used for "
-                                                                "icmake's\n"
-        "               temporary files. By default /tmp is used or the "
-                                                                "HOME\n"
-        "               directory if /tmp is not writable. When used, -T is\n"
-        "               usually specified as icmake's first option\n"
+                                                "icm-exec\n"
+
         "      -v     : display ",
-            program,
-                   "'s version number and end ",
-            program,
-                    "\n"
+                                   program,
+                                                "'s version number and end ",
+                                   program,
+                                            "\n"
         "\n"
+
         "    source: icmake source file to process (default extension: .im,\n"
         "            with -b the default extension is .bim)\n"
         "    dest:   generated binary icmake file (default: source.bim)\n"
